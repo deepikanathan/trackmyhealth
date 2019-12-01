ed639631a3c4be2937c3fdf94c714e3e6560d848
deepikanathan <dvarun83@gmail.com>
2019-12-01 09:33:00 -0600

    added height and weight spinners in SignUp form

---
+++ app/src/main/java/com/udacity/capstone/trackmyhealth/ui/LandingActivity.java
@@ -0,0 +1,37 @@
+package com.udacity.capstone.trackmyhealth.ui;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.udacity.capstone.trackmyhealth.R;
+
+public class LandingActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_landing);
+
+        Button signUpButton = findViewById(R.id.sign_up_button);
+
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                InvokeSignUpActivity();
+            }
+        });
+
+    }
+
+    private void InvokeSignUpActivity()
+    {
+        Intent intent = new Intent(this, SignUpActivity.class);
+        startActivity(intent);
+    }
+
+}
--- app/src/main/java/com/udacity/capstone/trackmyhealth/ui/SignUpActivity.java
+++ app/src/main/java/com/udacity/capstone/trackmyhealth/ui/SignUpActivity.java
@@ -1,44 +1,68 @@
 package com.udacity.capstone.trackmyhealth.ui;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.udacity.capstone.trackmyhealth.R;
-import com.udacity.capstone.trackmyhealth.utils.CustomOnItemSelectedListener;
 
-public class SignUpActivity extends AppCompatActivity {
+public class SignUpActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_signup);
 
-//        Spinner weight_spinner = (Spinner) findViewById(R.id.weight_unit_spinner);
-//        // Create an ArrayAdapter using the string array and a default spinner layout
-//        ArrayAdapter<CharSequence> weightadapter = ArrayAdapter.createFromResource(this,
-//                R.array.weight_units_array, android.R.layout.simple_spinner_item);
-//        // Specify the layout to use when the list of choices appears
-//        weightadapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-//        // Apply the adapter to the spinner
-//        weight_spinner.setAdapter(weightadapter);
-//        weight_spinner.setOnItemSelectedListener(new CustomOnItemSelectedListener());
-//
-//
-//
-//
-//
-//        Spinner height_spinner = (Spinner) findViewById(R.id.height_unit_spinner);
-//        // Create an ArrayAdapter using the string array and a default spinner layout
-//        ArrayAdapter<CharSequence> heightadapter = ArrayAdapter.createFromResource(this,
-//                R.array.weight_units_array, android.R.layout.simple_spinner_item);
-//        // Specify the layout to use when the list of choices appears
-//        heightadapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-//        // Apply the adapter to the spinner
-//        height_spinner.setAdapter(heightadapter);
-//        height_spinner.setOnItemSelectedListener(new CustomOnItemSelectedListener());
+        // Spinner element
+        Spinner heightSpinner = (Spinner) findViewById(R.id.height_unit_spinner);
+
+        // Spinner click listener
+        heightSpinner.setOnItemSelectedListener(this);
+
+        // Creating adapter for spinner
+
+        ArrayAdapter<CharSequence> heightDataAdapter =  ArrayAdapter.createFromResource(this, R.array.height_units_array, android.R.layout.simple_spinner_item);
+
+        // Drop down layout style - list view with radio button
+        heightDataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // attaching data adapter to spinner
+        heightSpinner.setAdapter(heightDataAdapter);
+
+
+        // Spinner element
+        Spinner weightSpinner = (Spinner) findViewById(R.id.weight_unit_spinner);
+
+        // Spinner click listener
+        weightSpinner.setOnItemSelectedListener(this);
+
+        // Creating adapter for spinner
+
+        ArrayAdapter<CharSequence> weightDataAdapter =  ArrayAdapter.createFromResource(this, R.array.weight_units_array, android.R.layout.simple_spinner_item);
+
+        // Drop down layout style - list view with radio button
+        weightDataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // attaching data adapter to spinner
+        weightSpinner.setAdapter(weightDataAdapter);
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        // On selecting a spinner item
+        String item = parent.getItemAtPosition(position).toString();
+
+        // Showing selected spinner item
+        Toast.makeText(parent.getContext(), "Selected: " + item, Toast.LENGTH_LONG).show();
+    }
+    public void onNothingSelected(AdapterView<?> arg0) {
+        // TODO Auto-generated method stub
     }
 }
